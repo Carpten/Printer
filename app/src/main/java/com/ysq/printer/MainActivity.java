d03afd0fc284d665d14846a3ee038bd01f325372
杨水强 <yangshuiqiang@chuangjiangx.com>
2018-05-31 16:28:47 +0800

    银联商务

--- app/src/main/java/com/ysq/printer/MainActivity.java
+++ app/src/main/java/com/ysq/printer/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        new PrintManage(new LklPrinter(MainActivity.this)).printDetail();
+        new PrintManage(new ChinaumsPrinter(MainActivity.this)).printDetail();
+        new PrintManage(new ChinaumsPrinter(MainActivity.this)).printDetail();
+        new PrintManage(new ChinaumsPrinter(MainActivity.this)).printDetail();
+        new PrintManage(new ChinaumsPrinter(MainActivity.this)).printDetail();
     }
 
     public void print(View view) {
